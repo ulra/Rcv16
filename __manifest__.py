a896a720430820d14186e4abfce98ba58d301451
Raul <ra.do.nu20@gmail.com>
2025-07-25 15:07:18 -0400

    rcv16_1.2.3

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'La Venezolana de Seguros y Vida',
-    'version': '16.0.2.5.0',
+    'version': '16.0.2.6.0',
     'category': 'Insurance',
     'summary': 'Gestión de Pólizas de Seguros Vehiculares',
     'description': """
@@ -21,6 +21,7 @@
     'depends': ['base', 'mail', 'web'],
     'data': [
         'security/ir.model.access.csv',
+        'security/design_generator_security.xml',
         'data/sequence_data.xml',
         'data/config_data.xml',
         'views/poliza_views.xml',
